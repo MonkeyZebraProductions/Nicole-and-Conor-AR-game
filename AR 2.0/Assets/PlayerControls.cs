b1100ded876b5bc072541fbccf96a4f2ed649f8b
MonkeyZebraProductions <conormulgrewmzp@gmail.com>
2020-11-10 17:59:38 +0000

    Added Wallrun

---
+++ AR 2.0/Assets/PlayerControls.cs
@@ -0,0 +1,205 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/PlayerControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerControls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerControls"",
+    ""maps"": [
+        {
+            ""name"": ""Gameplay"",
+            ""id"": ""a3007b4f-89ff-4200-8ab5-7de4985de094"",
+            ""actions"": [
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""3f3b2295-e103-4a57-ac73-eebf54158038"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""90c910dd-bec7-49de-9c78-d6d0a23d82a9"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""9d8b3ac6-8e4c-4990-bb32-30925b9d58e9"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""f4761d9d-494e-4b64-be31-96f3634c71b4"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""4c957bb7-c659-4f13-aa81-297243848834"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""2e910c86-8788-4dae-9ec6-ab498639914d"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""5636c690-3cf4-43cb-bb0e-b70b0f695dd6"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""51865066-12b0-4ccf-8233-21ca6ade9046"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Gameplay
+        m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
+        m_Gameplay_Jump = m_Gameplay.FindAction("Jump", throwIfNotFound: true);
+        m_Gameplay_Move = m_Gameplay.FindAction("Move", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Gameplay
+    private readonly InputActionMap m_Gameplay;
+    private IGameplayActions m_GameplayActionsCallbackInterface;
+    private readonly InputAction m_Gameplay_Jump;
+    private readonly InputAction m_Gameplay_Move;
+    public struct GameplayActions
+    {
+        private @PlayerControls m_Wrapper;
+        public GameplayActions(@PlayerControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Jump => m_Wrapper.m_Gameplay_Jump;
+        public InputAction @Move => m_Wrapper.m_Gameplay_Move;
+        public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GameplayActions set) { return set.Get(); }
+        public void SetCallbacks(IGameplayActions instance)
+        {
+            if (m_Wrapper.m_GameplayActionsCallbackInterface != null)
+            {
+                @Jump.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnJump;
+                @Move.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+            }
+            m_Wrapper.m_GameplayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+            }
+        }
+    }
+    public GameplayActions @Gameplay => new GameplayActions(this);
+    public interface IGameplayActions
+    {
+        void OnJump(InputAction.CallbackContext context);
+        void OnMove(InputAction.CallbackContext context);
+    }
+}
---
+++ AR 2.0/Assets/PlayerMovement.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerMovement : MonoBehaviour
+{
+    private PlayerControls controls;
+
+    [SerializeField] public float MovementSpeed = 5f;
+    [SerializeField] public float JumpSpeed = 5f;
+    [SerializeField] public float Gravity = 5f;
+
+    private Vector2 move;
+    private bool _isWalled, _isJumping, _isDashing;
+    private CharacterController characterController;
+    private float _jumpMultiplyer = 1;
+    private float _jumps,_jumpAduster,_wallJumpAdjuster;
+
+
+    public LayerMask whatIsWall;
+    public float wallrunForce, maxWallrunTime, maxWallSpeed;
+    bool isWallRight, isWallLeft;
+    bool isWallRunning;
+    public float maxWallRunCameraTilt, wallRunCameraTilt;
+
+
+    private void Awake()
+    {
+        controls = new PlayerControls();
+        characterController = GetComponent<CharacterController>();
+        controls.Gameplay.Jump.started += context => Jump();
+        controls.Gameplay.Jump.canceled += context => JumpCancel();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _jumps = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        move = controls.Gameplay.Move.ReadValue<Vector2>();
+
+        Vector3 movement = (move.y * transform.forward) + (move.x * transform.right);
+        characterController.Move(movement * MovementSpeed * Time.deltaTime);
+
+        if (characterController.isGrounded == false)
+        {
+            characterController.Move(new Vector3(0f, Gravity * -1 * Time.deltaTime, 0f));
+           
+        }
+        if (characterController.isGrounded == true)
+        {
+            
+            _jumps = 1;
+
+        }
+        CheckForWall();
+        if((isWallRight||isWallLeft) && characterController.isGrounded == false)
+        {
+            StartWallrun();
+            
+        }
+        if (_isJumping == true)
+        {
+            characterController.Move(new Vector3(JumpSpeed * _jumpMultiplyer * _wallJumpAdjuster * Time.deltaTime, JumpSpeed * _jumpMultiplyer * Time.deltaTime, 0f));
+
+
+            _jumpMultiplyer *= _jumpAduster;
+            if (_jumpMultiplyer <= 0.1f )
+            {
+                _isJumping = false;
+            }
+        }
+    }
+
+    private void Jump()
+    {
+        if (_jumps > 0)
+        {
+            _jumpMultiplyer = 1f;
+            _isJumping = true;
+            _jumps -= 1;
+            _jumpAduster=0.9f;
+            if(isWallLeft)
+            {
+                _wallJumpAdjuster = 3.0f;
+            }
+            else if(isWallRight)
+            {
+                _wallJumpAdjuster = -3.0f;
+            }
+            else
+            {
+                _wallJumpAdjuster = 0.0f;
+            }
+            //JumpSound.Play();
+        }
+
+    }
+
+    private void JumpCancel()
+    {
+        _jumpAduster = 0.5f;
+    }
+    private void StartWallrun()
+    {
+        
+        isWallRunning = true;
+        //allowDashForceCounter = false;
+        //_isJumping = true;
+        Gravity = 0.0f;
+        if (characterController.velocity.magnitude <= maxWallSpeed)
+        {
+            characterController.Move(transform.forward * Time.deltaTime* maxWallSpeed);
+
+            //Make sure char sticks to wall
+            //if (isWallRight)
+            //    rb.AddForce(orientation.right * wallrunForce / 5 * Time.deltaTime);
+            //else
+            //    rb.AddForce(-orientation.right * wallrunForce / 5 * Time.deltaTime);
+        }
+    }
+    private void StopWallRun()
+    {
+        isWallRunning = false;
+        Gravity = 5.0f;
+    }
+    private void CheckForWall() //make sure to call in void Update
+    {
+        isWallRight = Physics.Raycast(transform.position, characterController.transform.right, 1f, whatIsWall);
+        isWallLeft = Physics.Raycast(transform.position, -characterController.transform.right, 1f, whatIsWall);
+
+        //leave wall run
+        if (!isWallLeft && !isWallRight)
+        {
+            StopWallRun();
+        }
+
+
+        //reset double jump (if you have one :D)
+        if (isWallLeft || isWallRight)
+        {
+            _jumps = 1;
+        }
+        
+    }
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+}
