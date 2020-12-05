6b9b2df351e0271657e86ec68c06e9ed3f227d54
MonkeyZebraProductions <conormulgrewmzp@gmail.com>
2020-12-05 16:22:33 +0000

    Old Input and click a specific object

--- AR 2.0/Assets/PlayerControls.cs
+++ AR 2.0/Assets/PlayerControls.cs
@@ -125,6 +125,74 @@ public class @PlayerControls : IInputActionCollection, IDisposable
                     ""isPartOfComposite"": false
                 }
             ]
+        },
+        {
+            ""name"": ""Touch Controls"",
+            ""id"": ""22cddbeb-b766-4367-af44-892c5520e5cb"",
+            ""actions"": [
+                {
+                    ""name"": ""Tap"",
+                    ""type"": ""Button"",
+                    ""id"": ""d6951238-c3a2-4d33-8fce-e849bc743c07"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Tap Position"",
+                    ""type"": ""Value"",
+                    ""id"": ""bac72ecb-26ad-4c68-af5c-1afe5352191a"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""1186ef71-3dd8-47b2-bbe9-bc7037b1b508"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Tap"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""3645e19e-ed93-45b5-a173-f93a04d6242a"",
+                    ""path"": ""<Touchscreen>/press"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Tap"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""9d372a33-471d-40fc-8e4c-e9105e7b6838"",
+                    ""path"": ""<Mouse>/position"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Tap Position"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""f5864797-c264-4d15-bd44-a8450a0517e6"",
+                    ""path"": ""<Touchscreen>/position"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Tap Position"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
         }
     ],
     ""controlSchemes"": []
@@ -133,6 +201,10 @@ public class @PlayerControls : IInputActionCollection, IDisposable
         m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
         m_Gameplay_Jump = m_Gameplay.FindAction("Jump", throwIfNotFound: true);
         m_Gameplay_Move = m_Gameplay.FindAction("Move", throwIfNotFound: true);
+        // Touch Controls
+        m_TouchControls = asset.FindActionMap("Touch Controls", throwIfNotFound: true);
+        m_TouchControls_Tap = m_TouchControls.FindAction("Tap", throwIfNotFound: true);
+        m_TouchControls_TapPosition = m_TouchControls.FindAction("Tap Position", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -219,9 +291,55 @@ public class @PlayerControls : IInputActionCollection, IDisposable
         }
     }
     public GameplayActions @Gameplay => new GameplayActions(this);
+
+    // Touch Controls
+    private readonly InputActionMap m_TouchControls;
+    private ITouchControlsActions m_TouchControlsActionsCallbackInterface;
+    private readonly InputAction m_TouchControls_Tap;
+    private readonly InputAction m_TouchControls_TapPosition;
+    public struct TouchControlsActions
+    {
+        private @PlayerControls m_Wrapper;
+        public TouchControlsActions(@PlayerControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Tap => m_Wrapper.m_TouchControls_Tap;
+        public InputAction @TapPosition => m_Wrapper.m_TouchControls_TapPosition;
+        public InputActionMap Get() { return m_Wrapper.m_TouchControls; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(TouchControlsActions set) { return set.Get(); }
+        public void SetCallbacks(ITouchControlsActions instance)
+        {
+            if (m_Wrapper.m_TouchControlsActionsCallbackInterface != null)
+            {
+                @Tap.started -= m_Wrapper.m_TouchControlsActionsCallbackInterface.OnTap;
+                @Tap.performed -= m_Wrapper.m_TouchControlsActionsCallbackInterface.OnTap;
+                @Tap.canceled -= m_Wrapper.m_TouchControlsActionsCallbackInterface.OnTap;
+                @TapPosition.started -= m_Wrapper.m_TouchControlsActionsCallbackInterface.OnTapPosition;
+                @TapPosition.performed -= m_Wrapper.m_TouchControlsActionsCallbackInterface.OnTapPosition;
+                @TapPosition.canceled -= m_Wrapper.m_TouchControlsActionsCallbackInterface.OnTapPosition;
+            }
+            m_Wrapper.m_TouchControlsActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Tap.started += instance.OnTap;
+                @Tap.performed += instance.OnTap;
+                @Tap.canceled += instance.OnTap;
+                @TapPosition.started += instance.OnTapPosition;
+                @TapPosition.performed += instance.OnTapPosition;
+                @TapPosition.canceled += instance.OnTapPosition;
+            }
+        }
+    }
+    public TouchControlsActions @TouchControls => new TouchControlsActions(this);
     public interface IGameplayActions
     {
         void OnJump(InputAction.CallbackContext context);
         void OnMove(InputAction.CallbackContext context);
+    }
+    public interface ITouchControlsActions
+    {
+        void OnTap(InputAction.CallbackContext context);
+        void OnTapPosition(InputAction.CallbackContext context);
     }
 }
---
+++ AR 2.0/Assets/Scripts/Swich.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Swich : MonoBehaviour
+{
+    public Transform StartPos, EndPos;
+
+    private bool _switch;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = StartPos.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (_switch)
+        {
+            transform.position = EndPos.position;
+        }
+        else
+        {
+            transform.position = StartPos.position;     
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        _switch = !_switch;
+    }
+
+}
